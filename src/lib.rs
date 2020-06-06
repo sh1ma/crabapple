58ce9bdfb51694510eed69e1ec805d1953dbc2a0
luxx4x <luxx4x@protonmail.com>
2020-06-06 13:13:31 -0400

    e

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,21 @@ pub static mut ORIGIMP: Option<Imp> = None;
 extern "C" {
     fn OBJC_NSString(str: *const c_char) -> *mut c_void;
     fn OBJC_NSLog(str: *const c_char);
-    fn NSLogv(nsFormat: *mut c_void); // formast from inside rust or it dies
+    fn NSLogv(nsFormat: *mut c_void); // format from inside rust or it dies
 }
 
 #[inline(always)]
 fn to_c_str(s: &str) -> *const c_char {
-    let mut bytes = String::from(str).into_bytes();
+    let mut bytes = String::from(s).into_bytes();
     bytes.push(0);
     let ptr = bytes.as_ptr();
     std::mem::forget(bytes);
     unsafe { std::ffi::CStr::from_ptr(ptr as *const c_string::c_char).as_ptr() }
+}
+
+#[inline(always)]
+fn to_nsstr(s: &str) -> *const c_void {
+    unsafe { OBJC_NSString(to_c_str(s)) }
 }
 
 type set_background_alpha = unsafe extern "C" fn(this: &Object, _cmd: Sel, alpha: f64) -> c_double;
@@ -40,7 +45,7 @@ static LOAD: extern "C" fn() = {
             // then we can transmute it to change its type
             let swizz_imp: Imp = std::mem::transmute(f);
             OBJC_NSLog(to_c_str(&format!(
-                "method = {:#?}, f = {:#?}, swizz_imp = {:#?}",
+                "ReachCCRust: method = {:#?}, f = {:#?}, swizz_imp = {:#?}",
                 std::mem::transmute::<*mut Method, *mut c_void>(method),
                 std::mem::transmute::<set_background_alpha, *mut c_void>(f),
                 std::mem::transmute::<Imp, *mut c_void>(swizz_imp)
