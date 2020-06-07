72e74e7f1978ae77dd898a935dfa50335793ca45
luxx4x <luxx4x@protonmail.com>
2020-06-07 16:56:11 -0400

    Progress!

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use objc::runtime::*;
 use objc::*;
 use std::os::raw::{c_char, c_double, c_void};
+use std::mem::MaybeUninit;
 
 pub static mut ORIGIMP: Option<Imp> = None;
 
@@ -8,7 +9,7 @@ extern "C" {
     fn OBJC_NSString(str: *const c_char) -> *mut c_void;
     fn OBJC_NSLog(str: *const c_char);
     fn NSLogv(nsFormat: *mut c_void); // format from inside rust or it dies
-    fn MSHookMessageEx(class: *mut c_void, selector: *mut c_void, replacement: *mut c_void, result: *mut c_void);
+    fn MSHookMessageEx(class: *mut c_void, selector: *mut c_void, replacement: *mut c_void, result: *mut Imp);
 }
 
 #[inline(always)]
@@ -25,17 +26,24 @@ fn to_nsstr(s: &str) -> *const c_void {
     unsafe { OBJC_NSString(to_c_str(s)) }
 }
 
-type set_background_alpha = unsafe extern "C" fn(this: &Object, _cmd: Sel, alpha: f64) -> c_double;
+type set_background_alpha = unsafe extern "C" fn(this: &Object, cmd: Sel, alpha: f64);
 
 #[no_mangle]
-extern "C" fn my_set_background_alpha(this: &Object, cmd: Sel, alpha: c_double) -> c_double {
+extern "C" fn my_set_background_alpha(this: &Object, cmd: Sel, alpha: c_double) {
     unsafe {
         OBJC_NSLog(to_c_str(&format!(
             "ReachCCRust my_set_background_alpha: this = {:#?}, cmd = {:#?}, alpha = {}",
             this, cmd, alpha
         )));
+        if let Some(orig) = ORIGIMP {
+            let x: set_background_alpha = std::mem::transmute(orig);
+            OBJC_NSLog(to_c_str(&format!(
+                "ReachCCRust my_set_background_alpha = {:#?}",
+                orig
+            )));
+            x(this, cmd, 0.0);
+        }
     }
-    return 0.0;
 }
 
 #[used]
@@ -47,17 +55,20 @@ static LOAD: extern "C" fn() = {
             let swizz_imp: *mut c_void = std::mem::transmute(f);
             let sb_dock_view: *mut c_void = std::mem::transmute(objc_getClass(to_c_str("SBDockView")));
             let sba_sel: *mut c_void = std::mem::transmute(sel!(setBackgroundAlpha:));
-            let mut replaced: *mut c_void = std::ptr::null_mut();
             OBJC_NSLog(to_c_str(&format!(
                 "ReachCCRust hooking: swizz_imp = {:#?}, sb_dock_view = {:#?}, sba_sel = {:#?}",
                 swizz_imp, sb_dock_view, sba_sel
             )));
-            MSHookMessageEx(sb_dock_view, sba_sel, swizz_imp, &mut replaced as *mut *mut c_void as *mut c_void);
+            let replaced= {
+                let mut x: MaybeUninit<Imp> = std::mem::MaybeUninit::uninit();
+                MSHookMessageEx(sb_dock_view, sba_sel, swizz_imp, x.as_mut_ptr());
+                x.assume_init()
+            };
             OBJC_NSLog(to_c_str(&format!(
                 "ReachCCRust hooking: replaced = {:#?}",
                 replaced
             )));
-            ORIGIMP = Some(std::mem::transmute::<*mut c_void, Imp>(replaced));
+            ORIGIMP = Some(replaced);
         }
     }
     ctor
